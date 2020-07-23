d61c366cf86b9e496ca352bd37857e36e0e9c58a
KArthick707 <68534141+KArthick707@users.noreply.github.com>
2020-07-23 17:39:41 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+str ="what we think we become; we are learning python we and we"
+sub_str = "we"
+list= [i for i in range(len(str)) if str.startswith(sub_str,i)]
+print("the index value is", list)
+
+
+# In[17]:
+
+
+str = "enter the dragon"
+print(str.isupper())
+
+
+# In[21]:
+
+
+str = "SLEEPINNG DOGS"
+print (str.isupper())
+
+
+# In[22]:
+
+
+str =  "friends"
+print(str.islower())
+
+
+# In[23]:
+
+
+str = "FRIENDS OF MINE"
+print(str.islower())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
